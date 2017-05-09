7dd1050b96e72bda5759b87e1a72d77ca12a1118
katemontha <katemontha@gmail.com>
2017-05-09 18:05:48 -0400

    added js

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 window.onload = function () {
 
-  var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; // array of letters to choose from
-  var wordBank = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']// array of words to be randomly selected
-  var word; // chosen word
-  var guess; // user guess
-  var movesLeft; // number of moves left
-  var blanks; // placeholder for letters to be guessed
+  var letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var guesses = [];
+  var movesLeft;
+  var guess;
 
+  var wordBank = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
+  var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+//create letter placeholders
+  var answerArray = [];
+  for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_ ";
+  };
+
+//create letter buttons
+  for(var i = 0; i < letterArray.length; i++)
+  {
+    $('#letterChoices').html("<div>");
+      for(var j = 0; j < letterArray[i].length; j++)
+      {
+        $('#letterChoices').html("<input type='button' value='" + letterArray[i][j] + "'/>");
+      }
+      $('#letterChoices').html("</div>");
+  }
+
+//show word spaces
+  $('#startButton').on('click', function() {
+    $('#spaces').html(answerArray);
+    $('')
+  });
+
+//reset game
+  $('#resetButton').on('click', function() {
+    $('#spaces').replace();
+  });
+}
 
 
