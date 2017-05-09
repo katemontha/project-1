8200bba1409d101ada2d9dbe55d1bcebb921d8ab
katemontha <katemontha@gmail.com>
2017-05-09 13:46:27 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+  var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; // array of letters to choose from
+  var wordBank = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']// array of words to be randomly selected
+  var word; // chosen word
+  var guess; // user guess
+  var movesLeft; // number of moves left
+  var blanks; // placeholder for letters to be guessed
+
+
+
