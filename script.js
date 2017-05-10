fac2a6c3a79847dead2ae9039319a16cd3001a5b
katemontha <katemontha@gmail.com>
2017-05-10 14:42:40 -0400

    successly passed placeholders array to onclick function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function () {
   var letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var moves = 10;
   var guess;
+  var placeHolders = [];
 
   var wordBank = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
   var word = wordBank[Math.floor(Math.random() * wordBank.length)];
@@ -10,12 +11,10 @@ window.onload = function () {
 //start game
   $('#startButton').one('click', function() {
     //replace letters in word with place holders
-    var placeHolders = [];
     for (var i = 0; i < word.length; i++) {
       placeHolders[i] = "_ ";
     };
     $('#spaces').html(placeHolders);
-
     //displays moves left
     $('.movesLeft').html(moves)
 
@@ -23,32 +22,25 @@ window.onload = function () {
     for(var i = 0; i < letterArray.length; i++) {
       $('.letterChoices').append("<button>" + letterArray[i] + "</button>");
     }
+    return placeHolders;
   });
 
 //onclick function
   $(".letterChoices").click(function(event) {
     //create variable for clicked letter
     guess = event.target.innerHTML;
-
     //check if word contains clicked letter
     if (word.indexOf(guess) > -1) {
       console.log("found");
+      console.log(placeHolders);
+
       for (var i = 0; i < word.length; i++) {
         if (word[i] === guess) {
-          placeHolders[i] === guess;
-          //$('#spaces').html(guess);
+          console.log(guess);
+          console.log(word);
+          console.log(i);
         }
       }
-
-
-/*var index = items.indexOf(3452);
-
-if (index !== -1) {
-    items[index] = 1010;
-}*/
-
-
-
     } else {
       //subtract 1 from moves left
       console.log('not found');
@@ -57,5 +49,4 @@ if (index !== -1) {
     }
   });
 }
-
 
