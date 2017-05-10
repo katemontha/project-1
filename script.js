fc805c4e8413aa62985a6f39cf4331731a510cd4
katemontha <katemontha@gmail.com>
2017-05-10 09:18:29 -0400

    changed html and css

--- script.js
+++ script.js
@@ -8,21 +8,28 @@ window.onload = function () {
   var wordBank = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY'];
   var word = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-//show word spaces
-  $('#startButton').on('click', function() {
+//start game
+  $('#startButton').one('click', function() {
+    //show place holders
     var placeHolders = [];
     for (var i = 0; i < word.length; i++) {
       placeHolders[i] = "_ ";
     };
     $('#spaces').html(placeHolders);
+
     //create letter buttons
     for(var i = 0; i < letterArray.length; i++) {
-      $('#letterChoices').append("<button>" + letterArray[i] + "</button>");
-  }
+      $('.letterChoices').append("<button id = '" + letterArray[i] + "'>" + letterArray[i] + "</button>");
+    }
   });
 
+ //assign
+ $("button").click(function() {
+      var guess = ($(this).text());
+      return guess;
+  });
 
-
+ console.log(guess)
 }
 
 
